d95db7cd08663945274d8c3327c2006c8eab9c64
Omer Jahangir <omerjahangir31@gmail.com>
2025-07-01 15:47:24 +0500

    first commit

---
+++ lib/call_slider_button.dart
@@ -0,0 +1,10 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/call_slider_button_widget.dart';
+
+
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ lib/src/call_slider_button_widget.dart
@@ -0,0 +1,230 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CallSliderButton extends StatefulWidget {
+  final VoidCallback onAccept;
+  final VoidCallback onDecline;
+
+  // Customization options
+  final String acceptText;
+  final String declineText;
+  final TextStyle? textStyle;
+  final Color acceptColor;
+  final Color declineColor;
+  final Color idleBackgroundColor;
+  final Color iconColor;
+  final IconData acceptIcon;
+  final IconData declineIcon;
+  final double height;
+  final double borderRadius;
+  final double iconSize;
+  final double iconRadius;
+
+  const CallSliderButton({
+    super.key,
+    required this.onAccept,
+    required this.onDecline,
+    this.acceptText = 'Accept',
+    this.declineText = 'Decline',
+    this.textStyle,
+    this.acceptColor = Colors.green,
+    this.declineColor = Colors.red,
+    this.idleBackgroundColor = const Color(0x22FFFFFF),
+    this.iconColor = Colors.green,
+    this.acceptIcon = Icons.call,
+    this.declineIcon = Icons.call_end,
+    this.height = 70,
+    this.borderRadius = 50,
+    this.iconSize = 35,
+    this.iconRadius = 30,
+  });
+
+  @override
+  State<CallSliderButton> createState() => _CallSliderButtonState();
+}
+
+class _CallSliderButtonState extends State<CallSliderButton>
+    with TickerProviderStateMixin {
+  double _dragPosition = 0.0;
+  final double _dragThreshold = 80.0;
+  bool _hapticTriggered = false;
+
+  late AnimationController _resetController;
+  late Animation<double> _resetAnimation;
+  late AnimationController _pulseController;
+  late Animation<double> _pulseAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _resetController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+
+    _pulseController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    )..repeat(reverse: true);
+
+    _pulseAnimation = Tween<double>(begin: 0.95, end: 1.1).animate(
+      CurvedAnimation(parent: _pulseController, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _resetController.dispose();
+    _pulseController.dispose();
+    super.dispose();
+  }
+
+  void _handleDragEnd(DragEndDetails details) {
+    if (_dragPosition > _dragThreshold) {
+      widget.onAccept();
+    } else if (_dragPosition < -_dragThreshold) {
+      widget.onDecline();
+    }
+    _animateReset();
+  }
+
+  void _animateReset() {
+    _resetAnimation = Tween<double>(
+      begin: _dragPosition,
+      end: 0.0,
+    ).animate(_resetController)..addListener(() {
+      setState(() {
+        _dragPosition = _resetAnimation.value;
+      });
+    });
+    _resetController.forward(from: 0.0);
+    _hapticTriggered = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isDragging = _dragPosition != 0;
+    final bool isDeclining = _dragPosition < 0;
+    final double containerWidth = MediaQuery.of(context).size.width * 0.8;
+    final double safeDragLimit = containerWidth * 0.39;
+
+    final double progress = (_dragPosition.abs() / safeDragLimit).clamp(
+      0.0,
+      1.0,
+    );
+    final Color bgColor =
+        _dragPosition > 0
+            ? widget.acceptColor.withValues(alpha: 0.2 + 0.6 * progress)
+            : _dragPosition < 0
+            ? widget.declineColor.withValues(alpha: 0.2 + 0.6 * progress)
+            : widget.idleBackgroundColor;
+    final Color iconColor =
+        isDeclining ? widget.declineColor : widget.iconColor;
+    final IconData icon = isDeclining ? widget.declineIcon : widget.acceptIcon;
+
+    if (!_hapticTriggered && (_dragPosition.abs() > _dragThreshold + 5)) {
+      HapticFeedback.mediumImpact();
+      _hapticTriggered = true;
+    }
+
+    return Center(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(widget.borderRadius),
+        child: Container(
+          width: containerWidth,
+          height: widget.height,
+          decoration: BoxDecoration(
+            color: bgColor,
+            borderRadius: BorderRadius.circular(widget.borderRadius),
+            border: Border.all(
+              color: bgColor.withValues(alpha: 0.2),
+              width: 1.5,
+            ),
+          ),
+          child: Stack(
+            clipBehavior: Clip.hardEdge,
+            alignment: Alignment.center,
+            children: [
+              if (!isDragging) ...[
+                AnimatedOpacity(
+                  duration: const Duration(milliseconds: 200),
+                  opacity: 1.0,
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: containerWidth * 0.09),
+                      child: Text(
+                        widget.declineText,
+                        style:
+                            widget.textStyle ??
+                            const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20,
+                            ),
+                      ),
+                    ),
+                  ),
+                ),
+                AnimatedOpacity(
+                  duration: const Duration(milliseconds: 200),
+                  opacity: 1.0,
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: EdgeInsets.only(right: containerWidth * 0.09),
+                      child: Text(
+                        widget.acceptText,
+                        style:
+                            widget.textStyle ??
+                            const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20,
+                            ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+              GestureDetector(
+                onHorizontalDragUpdate: (details) {
+                  setState(() {
+                    _dragPosition += details.delta.dx;
+                    _dragPosition = _dragPosition.clamp(
+                      -safeDragLimit,
+                      safeDragLimit,
+                    );
+                  });
+                },
+                onHorizontalDragEnd: _handleDragEnd,
+                child: Transform.translate(
+                  offset: Offset(_dragPosition, 0),
+                  child: AnimatedBuilder(
+                    animation: _pulseAnimation,
+                    builder: (context, child) {
+                      return Transform.scale(
+                        scale: _pulseAnimation.value,
+                        child: child,
+                      );
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: widget.iconRadius,
+                      child: Icon(
+                        icon,
+                        color: iconColor,
+                        size: widget.iconSize,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
